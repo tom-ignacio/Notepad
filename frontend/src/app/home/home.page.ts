bf5f1fe8a007018ba375d04da9f045d6502e8271
Ricardo <ricardodjsc@gmail.com>
2023-02-09 13:26:48 -0400

    integracion del metodo get en la vista home para obtener las notas del backend (de momento llama a todas la notas)

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  notes: any = []
+
+  constructor(private http : HttpClient) {}
+
+  loadNotes(){
+    this.http.get("http://localhost:3000/notepad")
+    .subscribe(res => this.notes = res , err => console.log(err))
+  }
+
+  ngOnInit() {
+    this.loadNotes();
+  }
+
+  ionViewWillEnter(){
+    this.loadNotes();
+  }
 
 }
