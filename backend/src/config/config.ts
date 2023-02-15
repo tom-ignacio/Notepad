861aff1ef003628bef3213a0bbead7d3047b3286
Ricardo <ricardodjsc@gmail.com>
2023-02-15 19:29:55 -0400

    implementacion mongoAtlas

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ export default {
   jwtSecret: process.env.JWT_SECRET || "somesecrettoken",
 
   DB: {
-    URI: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/notepadDB",
+    URI: process.env.MONGODB_URI || "mongodb+srv://moviles:123456moviles@cluster0.ggynxqb.mongodb.net/?retryWrites=true&w=majority",
     USER: process.env.MONGODB_USER || "",
     PASSWORD: process.env.MONGODB_PASSWORD || "",
     NAME: process.env.MONGODB_NAME || "",
